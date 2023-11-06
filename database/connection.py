08581212e292f74ac4643d33670ea78d20d88347
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-11-06 13:48:03 -0500

    Exclude mongo's id

--- database/connection.py
+++ database/connection.py
@@ -1,23 +1,43 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
 DATABASE_NAME = "project_db"
 COLLECTION_NAME = "products"
 
-
-def _get_connection_string()->str:
+def _get_connection_string() -> str:
     return os.getenv("CONNECTION_STRING")
 
-def _get_client_with(connection_string:str)->MongoClient:
-    return MongoClient(connection_string)
+def _get_client_with(connection_string: str) -> MongoClient:
+    CLIENT = MongoClient(connection_string)
+    return CLIENT 
 
-def _connect_to_mongo()->MongoClient:
+def _connect_to_mongo() -> MongoClient:
     connection_string = _get_connection_string()
     client = _get_client_with(connection_string)
     return client
 
-async def fetch_products():
+def _get_database_from(client: MongoClient)->MongoClient:
+    return client[DATABASE_NAME]
+
+def _get_collection_from(database: str)->MongoClient:
+    return database[COLLECTION_NAME]
+
+def fetch_products():
     client = _connect_to_mongo()
-    db = client[DATABASE_NAME]
-    collection = db[COLLECTION_NAME]
-    return await collection.find({})
+    database = _get_database_from(client)
+    collection = _get_collection_from(database)
+    return collection.find({},{'_id': 0}).limit(10)
+
+
+
+if __name__ == "__main__":
+    try:
+        products = fetch_products()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    load_dotenv()
+    products = fetch_products()
+    for product in products:
+        print(product)
