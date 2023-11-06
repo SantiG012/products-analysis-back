e3dc5bc840004c710cc5bed5b2ab2792dfdad78a
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-11-06 14:38:40 -0500

    Add a test endpoint

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Union
 from dotenv import load_dotenv
-
 from fastapi import FastAPI
+from  database.connection import fetch_products
+import pandas as pd
 
 app = FastAPI()
 
@@ -9,6 +10,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     load_dotenv()
+    products = fetch_products()
+    products = pd.DataFrame(products)
+    try:
+        return products.to_dict(orient="records")
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 
 @app.get("/items/{item_id}")
