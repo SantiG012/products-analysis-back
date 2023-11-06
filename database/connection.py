ddf57c033bc2151ca3732ac7eb457b10cf07c692
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-11-06 14:37:29 -0500

    Sort and limit data

--- database/connection.py
+++ database/connection.py
@@ -23,11 +23,21 @@ def _get_database_from(client: MongoClient)->MongoClient:
 def _get_collection_from(database: str)->MongoClient:
     return database[COLLECTION_NAME]
 
+def _get_pipeline() -> list:
+    pipeline = [
+        {'$sample': {'size': 30000}},
+        {'$project': {'_id': 0}}
+    ]
+    return pipeline
+
 def fetch_products():
     client = _connect_to_mongo()
     database = _get_database_from(client)
     collection = _get_collection_from(database)
-    return collection.find({},{'_id': 0}).limit(10)
+    pipeline = _get_pipeline()
+
+    return collection.aggregate(pipeline)
+
 
 
 
