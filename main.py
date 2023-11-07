ae09408b7e31a1c0b8fe958256e7eecc7392e3cd
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-11-07 08:41:04 -0500

    Add a test endpoint

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from  database.connection import fetch_products
 import pandas as pd
+from data_processing.correlations.rating_reviews import calculate_correlation_between
+from dtos.correlation_dto import Correlation_dto
 
 app = FastAPI()
 
@@ -12,8 +14,11 @@ def read_root():
     load_dotenv()
     products = fetch_products()
     products = pd.DataFrame(products)
+    correlation = calculate_correlation_between(products["stars"], products["price"])
+    correlation_df =products[['stars','price']]
+    correlation_dto = Correlation_dto(correlation=correlation,correlation_df=correlation_df.to_dict(orient='records'))
     try:
-        return products.to_dict(orient="records")
+        return correlation_dto
     except Exception as e:
         return f"An error occurred: {e}"
 
