869da427e3d105cb9cc79f4fc17ed7e08d2b1ec4
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-11-07 14:18:51 -0500

    Add exceptions

--- data_processing/correlations/rating_reviews.py
+++ data_processing/correlations/rating_reviews.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-def calculate_correlation_between(rating: pd.Series,reviews: pd.Series)->float:
-    return rating.corr(reviews)
+def calculate_correlation_between(series_a: pd.Series,series_b: pd.Series)->float:
+    if not _has_numeric_values(series_a) and not _has_numeric_values(series_b):
+       raise Exception("Series must contain numeric values")
+    
+    return series_a.corr(series_b)
+    
 
+
+def _has_numeric_values(series: pd.Series)->bool:
+    return series.dtype == "float64" or series.dtype == "int64"
--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 from typing import Union
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from  database.connection import fetch_products
 import pandas as pd
-from data_processing.correlations.rating_reviews import calculate_correlation_between
 from dtos.correlation_dto import Correlation_dto
+from data_processing.correlations.rating_reviews import calculate_correlation_between
 
 app = FastAPI()
+products = None
 
 
 @app.get("/")
-def read_root():
+def set_up():
     load_dotenv()
-    products = fetch_products()
-    products = pd.DataFrame(products)
-    correlation = calculate_correlation_between(products["stars"], products["price"])
-    correlation_df =products[['stars','price']]
-    correlation_dto = Correlation_dto(correlation=correlation,correlation_df=correlation_df.to_dict(orient='records'))
     try:
-        return correlation_dto
+        global products 
+        products = fetch_products()
+        products = pd.DataFrame(products)
+
+        return "Products fetched successfully!"
     except Exception as e:
         return f"An error occurred: {e}"
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/correlation/{column_a}/{column_b}")
+def read_item(column_a: str, column_b: str):
+    if not _products_fetched():
+        raise HTTPException(status_code=500, detail="Products must be fetched first")
+    
+    if not _column_exists(column_a) or not _column_exists(column_b):
+        raise HTTPException(status_code=404,detail=f"Either {column_a} or {column_b} does not exist")
+    
+    try:
+        correlation = calculate_correlation_between(products[column_a], products[column_b])
+        correlation_df = products[[column_a, column_b]]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
+    correlation_dto = Correlation_dto(correlation=correlation, correlation_df=correlation_df.to_dict(orient="records"))
+
+    return correlation_dto
+
+
+
+def _products_fetched():
+    return products is not None
+
+def _column_exists(column: str):
+    return column in products.columns
